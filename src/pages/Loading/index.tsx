86e3d3edfb908c41c484971e3f61e0275caf6d65
genebit <genebitara@gmail.com>
2024-02-18 00:30:23 +0800

    style: ui fixes

--- src/pages/Loading/index.tsx
+++ src/pages/Loading/index.tsx
@@ -1,6 +1,6 @@
 const LoadingPage = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-screen animate-in fade-in">
       <div className="relative inline-flex">
         <div className="w-8 h-8 rounded-full bg-rose-500"></div>
         <div className="absolute top-0 left-0 w-8 h-8 rounded-full bg-rose-500 animate-ping"></div>
