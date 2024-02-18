8f8222c546307b294908f254f99a5e829fbb2217
genebit <genebitara@gmail.com>
2024-02-19 00:57:16 +0800

    chore: code cleanup + add layout

---
+++ src/layouts/HomeLayout.tsx
@@ -0,0 +1,24 @@
+import { Fragment, HTMLAttributes } from "react"
+
+import splineCanvas from "@/assets/svgs/SplineCanvasSnapshot.svg"
+import TopNavbar from "@/pages/Home/components/TopNavbar/TopNavbar"
+import ContactSidebarWrapper from "@/pages/Home/components/ContactSidebar/ContactSidebarWrapper"
+
+import "@/pages/Home/index.css"
+
+const HomeLayout = ({ children }: HTMLAttributes<HTMLElement>) => {
+  return (
+    <Fragment>
+      <TopNavbar />
+      <div className="relative z-20 animate-in fade-in">
+        <ContactSidebarWrapper />
+        {children}
+      </div>
+      <div className="flex justify-center">
+        <img className="absolute top-0 z-0" src={splineCanvas} alt="" />
+      </div>
+    </Fragment>
+  )
+}
+
+export default HomeLayout
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,103 +1,88 @@
-import { Fragment } from "react"
-
 import ScrollSpy from "react-ui-scrollspy"
 
-import splineCanvas from "@/assets/svgs/SplineCanvasSnapshot.svg"
-
-import TopNavbar from "@/pages/Home/components/TopNavbar/TopNavbar"
+import HomeLayout from "@/layouts/HomeLayout"
+import RightSidebarHeading from "@/pages/Home/components/RightSidebar/RightSidebarHeading"
 import RightSidebarLink from "@/pages/Home/components/RightSidebar/RightSidebarLink"
 import RightSidebarWrapper from "@/pages/Home/components/RightSidebar/RightSidebarWrapper"
-import RightSidebarHeading from "@/pages/Home/components/RightSidebar/RightSidebarHeading"
-import ContactSidebarWrapper from "@/pages/Home/components/ContactSidebar/ContactSidebarWrapper"
-
-import "@/pages/Home/index.css"
 
 const HomePage = () => {
   return (
-    <Fragment>
-      <TopNavbar />
-      <div className="relative z-20 animate-in fade-in">
-        <ContactSidebarWrapper />
-        <article className="article-container">
-          <ScrollSpy>
-            <section data-section="ABOUT" id="about">
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error fugiat odit ducimus ea, asperiores
-                voluptas id in enim, culpa repudiandae modi voluptatum cumque qui illum, atque dolorum perspiciatis
-                facilis obcaecati? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error fugiat odit ducimus
-                ea, asperiores voluptas id in enim, culpa repudiandae modi voluptatum cumque qui illum, atque dolorum
-                perspiciatis facilis obcaecati? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error fugiat
-                odit ducimus ea, asperiores voluptas id in enim, culpa repudiandae modi voluptatum cumque qui illum,
-                atque dolorum perspiciatis facilis obcaecati? Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Error fugiat odit ducimus ea, asperiores voluptas id in enim, culpa repudiandae modi voluptatum cumque
-                qui illum, atque dolorum perspiciatis facilis obcaecati? Lorem, ipsum dolor sit amet consectetur
-                adipisicing elit. Error fugiat odit ducimus ea, asperiores voluptas id in enim, culpa repudiandae modi
-                voluptatum cumque qui illum, atque dolorum perspiciatis facilis obcaecati? Lorem, ipsum dolor sit amet
-                consectetur adipisicing elit. Error fugiat odit ducimus ea, asperiores voluptas id in enim, culpa
-                repudiandae modi voluptatum cumque qui illum, atque dolorum perspiciatis facilis obcaecati? Lorem, ipsum
-                dolor sit amet consectetur adipisicing elit. Error fugiat odit ducimus ea, asperiores voluptas id in
-                enim, culpa repudiandae modi voluptatum cumque qui illum, atque dolorum perspiciatis facilis obcaecati?
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error fugiat odit ducimus ea, asperiores
-                voluptas id in enim, culpa repudiandae modi voluptatum cumque qui illum, atque dolorum perspiciatis
-                facilis obcae Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error fugiat odit ducimus ea,
-                asperiores voluptas id in enim, culpa repudiandae modi voluptatum cumque qui illum, atque dolorum
-                perspiciatis facilis obcaecati? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error fugiat
-                odit ducimus ea, asperiores voluptas id in enim, culpa repudiandae modi voluptatum cumque qui illum,
-                atque dolorum perspiciatis facilis obcaecati? Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Error fugiat odit ducimus ea, asperiores voluptas id in enim, culpa repudiandae modi voluptatum cumque
-                qui illum, atque dolorum perspiciatis facilis obcaecati? Lorem, ipsum dolor sit amet consectetur
-                adipisicing elit. Error fugiat odit ducimus ea, asperiores voluptas id in enim, culpa repudiandae modi
-                voluptatum cumque qui illum, atque dolorum perspiciatis facilis obcae Lorem, ipsum dolor sit amet
-                consectetur adipisicing elit. Error fugiat odit ducimus ea, asperiores voluptas id in enim, culpa
-                repudiandae modi voluptatum cumque qui illum, atque dolorum perspiciatis facilis obcaecati? Lorem, ipsum
-                dolor sit amet consectetur adipisicing elit. Error fugiat odit ducimus ea, asperiores voluptas id in
-                enim, culpa repudiandae modi voluptatum cumque qui illum, atque dolorum perspiciatis facilis obcaecati?
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error fugiat odit ducimus ea, asperiores
-                voluptas id in enim, culpa repudiandae modi voluptatum cumque qui illum, atque dolorum perspiciatis
-                facilis obcaecati? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error fugiat odit ducimus
-                ea, asperiores voluptas id in enim, culpa repudiandae modi voluptatum cumque qui illum, atque dolorum
-                perspiciatis facilis obcae Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error fugiat odit
-                ducimus ea, asperiores voluptas id in enim, culpa repudiandae modi voluptatum cumque qui illum, atque
-                dolorum perspiciatis facilis obcaecati? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error
-                fugiat odit ducimus ea, asperiores voluptas id in enim, culpa repudiandae modi voluptatum cumque qui
-                illum, atque dolorum perspiciatis facilis obcaecati? Lorem, ipsum dolor sit amet consectetur adipisicing
-                elit. Error fugiat odit ducimus ea, asperiores voluptas id in enim, culpa repudiandae modi voluptatum
-                cumque qui illum, atque dolorum perspiciatis facilis obcaecati? Lorem, ipsum dolor sit amet consectetur
-                adipisicing elit. Error fugiat odit ducimus ea, asperiores voluptas id in enim, culpa repudiandae modi
-                voluptatum cumque qui illum, atque dolorum perspiciatis facilis obcae Lorem, ipsum dolor sit amet
-                consectetur adipisicing elit. Error fugiat odit ducimus ea, asperiores voluptas id in enim, culpa
-                repudiandae modi voluptatum cumque qui illum, atque dolorum perspiciatis facilis obcaecati? Lorem, ipsum
-                dolor sit amet consectetur adipisicing elit. Error fugiat odit ducimus ea, asperiores voluptas id in
-                enim, culpa repudiandae modi voluptatum cumque qui illum, atque dolorum perspiciatis facilis obcaecati?
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error fugiat odit ducimus ea, asperiores
-                voluptas id in enim, culpa repudiandae modi voluptatum cumque qui illum, atque dolorum perspiciatis
-                facilis obcaecati? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error fugiat odit ducimus
-                ea, asperiores voluptas id in enim, culpa repudiandae modi voluptatum cumque qui illum, atque dolorum
-                perspiciatis facilis obcae Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error fugiat odit
-                ducimus ea, asperiores voluptas id in enim, culpa repudiandae modi voluptatum cumque qui illum, atque
-                dolorum perspiciatis facilis obcaecati? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error
-                fugiat odit ducimus ea, asperiores voluptas id in enim, culpa repudiandae modi voluptatum cumque qui
-                illum, atque dolorum perspiciatis facilis obcaecati? Lorem, ipsum dolor sit amet consectetur adipisicing
-                elit. Error fugiat odit ducimus ea, asperiores voluptas id in enim, culpa repudiandae modi voluptatum
-                cumque qui illum, atque dolorum perspiciatis facilis obcaecati? Lorem, ipsum dolor sit amet consectetur
-                adipisicing elit. Error fugiat odit ducimus ea, asperiores voluptas id in enim, culpa repudiandae modi
-                voluptatum cumque qui illum, atque dolorum perspiciatis facilis obcaecati? Lorem, ipsum dolor sit amet
-                consectetur adipisicing elit. Error fugiat odit ducimus
-              </p>
-            </section>
-          </ScrollSpy>
-        </article>
-        <RightSidebarWrapper>
-          <RightSidebarHeading>ON THIS PAGE</RightSidebarHeading>
-          <RightSidebarLink jumpTo="about" label="ABOUT" />
-          <RightSidebarLink jumpTo="toolsIUse" label="TOOLS I USE" />
-          <RightSidebarLink jumpTo="projects" label="PROJECTS" />
-        </RightSidebarWrapper>
-      </div>
-      <div className="flex justify-center">
-        <img className="absolute top-0 z-0" src={splineCanvas} alt="" />
-      </div>
-    </Fragment>
+    <HomeLayout>
+      <article className="article-container">
+        <ScrollSpy>
+          <section data-section="ABOUT" id="about">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error fugiat odit ducimus ea, asperiores
+              voluptas id in enim, culpa repudiandae modi voluptatum cumque qui illum, atque dolorum perspiciatis
+              facilis obcaecati? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error fugiat odit ducimus ea,
+              asperiores voluptas id in enim, culpa repudiandae modi voluptatum cumque qui illum, atque dolorum
+              perspiciatis facilis obcaecati? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error fugiat
+              odit ducimus ea, asperiores voluptas id in enim, culpa repudiandae modi voluptatum cumque qui illum, atque
+              dolorum perspiciatis facilis obcaecati? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error
+              fugiat odit ducimus ea, asperiores voluptas id in enim, culpa repudiandae modi voluptatum cumque qui
+              illum, atque dolorum perspiciatis facilis obcaecati? Lorem, ipsum dolor sit amet consectetur adipisicing
+              elit. Error fugiat odit ducimus ea, asperiores voluptas id in enim, culpa repudiandae modi voluptatum
+              cumque qui illum, atque dolorum perspiciatis facilis obcaecati? Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Error fugiat odit ducimus ea, asperiores voluptas id in enim, culpa repudiandae modi
+              voluptatum cumque qui illum, atque dolorum perspiciatis facilis obcaecati? Lorem, ipsum dolor sit amet
+              consectetur adipisicing elit. Error fugiat odit ducimus ea, asperiores voluptas id in enim, culpa
+              repudiandae modi voluptatum cumque qui illum, atque dolorum perspiciatis facilis obcaecati? Lorem, ipsum
+              dolor sit amet consectetur adipisicing elit. Error fugiat odit ducimus ea, asperiores voluptas id in enim,
+              culpa repudiandae modi voluptatum cumque qui illum, atque dolorum perspiciatis facilis obcae Lorem, ipsum
+              dolor sit amet consectetur adipisicing elit. Error fugiat odit ducimus ea, asperiores voluptas id in enim,
+              culpa repudiandae modi voluptatum cumque qui illum, atque dolorum perspiciatis facilis obcaecati? Lorem,
+              ipsum dolor sit amet consectetur adipisicing elit. Error fugiat odit ducimus ea, asperiores voluptas id in
+              enim, culpa repudiandae modi voluptatum cumque qui illum, atque dolorum perspiciatis facilis obcaecati?
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error fugiat odit ducimus ea, asperiores
+              voluptas id in enim, culpa repudiandae modi voluptatum cumque qui illum, atque dolorum perspiciatis
+              facilis obcaecati? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error fugiat odit ducimus ea,
+              asperiores voluptas id in enim, culpa repudiandae modi voluptatum cumque qui illum, atque dolorum
+              perspiciatis facilis obcae Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error fugiat odit
+              ducimus ea, asperiores voluptas id in enim, culpa repudiandae modi voluptatum cumque qui illum, atque
+              dolorum perspiciatis facilis obcaecati? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error
+              fugiat odit ducimus ea, asperiores voluptas id in enim, culpa repudiandae modi voluptatum cumque qui
+              illum, atque dolorum perspiciatis facilis obcaecati? Lorem, ipsum dolor sit amet consectetur adipisicing
+              elit. Error fugiat odit ducimus ea, asperiores voluptas id in enim, culpa repudiandae modi voluptatum
+              cumque qui illum, atque dolorum perspiciatis facilis obcaecati? Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Error fugiat odit ducimus ea, asperiores voluptas id in enim, culpa repudiandae modi
+              voluptatum cumque qui illum, atque dolorum perspiciatis facilis obcae Lorem, ipsum dolor sit amet
+              consectetur adipisicing elit. Error fugiat odit ducimus ea, asperiores voluptas id in enim, culpa
+              repudiandae modi voluptatum cumque qui illum, atque dolorum perspiciatis facilis obcaecati? Lorem, ipsum
+              dolor sit amet consectetur adipisicing elit. Error fugiat odit ducimus ea, asperiores voluptas id in enim,
+              culpa repudiandae modi voluptatum cumque qui illum, atque dolorum perspiciatis facilis obcaecati? Lorem,
+              ipsum dolor sit amet consectetur adipisicing elit. Error fugiat odit ducimus ea, asperiores voluptas id in
+              enim, culpa repudiandae modi voluptatum cumque qui illum, atque dolorum perspiciatis facilis obcaecati?
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error fugiat odit ducimus ea, asperiores
+              voluptas id in enim, culpa repudiandae modi voluptatum cumque qui illum, atque dolorum perspiciatis
+              facilis obcae Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error fugiat odit ducimus ea,
+              asperiores voluptas id in enim, culpa repudiandae modi voluptatum cumque qui illum, atque dolorum
+              perspiciatis facilis obcaecati? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error fugiat
+              odit ducimus ea, asperiores voluptas id in enim, culpa repudiandae modi voluptatum cumque qui illum, atque
+              dolorum perspiciatis facilis obcaecati? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error
+              fugiat odit ducimus ea, asperiores voluptas id in enim, culpa repudiandae modi voluptatum cumque qui
+              illum, atque dolorum perspiciatis facilis obcaecati? Lorem, ipsum dolor sit amet consectetur adipisicing
+              elit. Error fugiat odit ducimus ea, asperiores voluptas id in enim, culpa repudiandae modi voluptatum
+              cumque qui illum, atque dolorum perspiciatis facilis obcae Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Error fugiat odit ducimus ea, asperiores voluptas id in enim, culpa repudiandae modi
+              voluptatum cumque qui illum, atque dolorum perspiciatis facilis obcaecati? Lorem, ipsum dolor sit amet
+              consectetur adipisicing elit. Error fugiat odit ducimus ea, asperiores voluptas id in enim, culpa
+              repudiandae modi voluptatum cumque qui illum, atque dolorum perspiciatis facilis obcaecati? Lorem, ipsum
+              dolor sit amet consectetur adipisicing elit. Error fugiat odit ducimus ea, asperiores voluptas id in enim,
+              culpa repudiandae modi voluptatum cumque qui illum, atque dolorum perspiciatis facilis obcaecati? Lorem,
+              ipsum dolor sit amet consectetur adipisicing elit. Error fugiat odit ducimus ea, asperiores voluptas id in
+              enim, culpa repudiandae modi voluptatum cumque qui illum, atque dolorum perspiciatis facilis obcaecati?
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error fugiat odit ducimus
+            </p>
+          </section>
+        </ScrollSpy>
+      </article>
+      <RightSidebarWrapper>
+        <RightSidebarHeading>ON THIS PAGE</RightSidebarHeading>
+        <RightSidebarLink jumpTo="about" label="ABOUT" />
+        <RightSidebarLink jumpTo="toolsIUse" label="TOOLS I USE" />
+        <RightSidebarLink jumpTo="projects" label="PROJECTS" />
+      </RightSidebarWrapper>
+    </HomeLayout>
   )
 }
 
