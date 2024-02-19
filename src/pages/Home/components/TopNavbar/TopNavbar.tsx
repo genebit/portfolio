3668cfd869bbaa4a5860188624c0fd0917ee1081
genebit <genebitara@gmail.com>
2024-02-19 18:56:59 +0800

    implement theme mode

--- src/pages/Home/components/TopNavbar/TopNavbar.tsx
+++ src/pages/Home/components/TopNavbar/TopNavbar.tsx
@@ -1,6 +1,6 @@
 import { HTMLAttributes } from "react"
 
-import { MoonStar, UserRoundSearch } from "lucide-react"
+import { MoonStar, Sun, UserRoundSearch } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
@@ -8,6 +8,8 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 
 import RightSidebarLink from "@/pages/Home/components/RightSidebar/RightSidebarLink"
 import ContactSidebarWrapper from "@/pages/Home/components/ContactSidebar/ContactSidebarWrapper"
+import { useTheme } from "@/components/ThemeProvider"
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@/components/ui/dropdown-menu"
 
 /**
  * Represents the top navigation bar component.
@@ -19,22 +21,47 @@ import ContactSidebarWrapper from "@/pages/Home/components/ContactSidebar/Contac
 const TopNavbar = ({ children, className, ...props }: HTMLAttributes<HTMLElement>) => {
   const compCn = cn("fixed z-30 flex w-full px-5 py-2 bg-transparent backdrop-filter backdrop-blur", className)
 
+  const ToggleContactSidebar = () => {
+    return (
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant="ghost" size="icon" className="bg-transparent lg:hidden text-primary animate-in fade-in">
+            <UserRoundSearch size={20} />
+          </Button>
+        </SheetTrigger>
+        <SheetContent className="w-max" side="left">
+          <ContactSidebarWrapper className="static block" />
+        </SheetContent>
+      </Sheet>
+    )
+  }
+
+  const ToggleThemeMode = () => {
+    const { setTheme } = useTheme()
+
+    return (
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" size="icon" className="bg-transparent">
+            <Sun className="h-[1.2rem] w-[1.2rem] text-primary rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <MoonStar className="absolute h-[1.2rem] text-primary w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <span className="sr-only">Toggle theme</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem onClick={() => setTheme("light")}>Light</DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("dark")}>Dark</DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("system")}>System</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    )
+  }
+
   return (
     <nav className={compCn} {...props}>
       <div className="flex">
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button variant="ghost" size="icon">
-              <UserRoundSearch size={20} className="lg:hidden text-primary animate-in fade-in" />
-            </Button>
-          </SheetTrigger>
-          <SheetContent className="w-max" side="left">
-            <ContactSidebarWrapper className="static block" />
-          </SheetContent>
-        </Sheet>
-        <Button variant="ghost" size="icon">
-          <MoonStar size={20} className="text-primary" />
-        </Button>
+        <ToggleContactSidebar />
+        <ToggleThemeMode />
       </div>
       <div className="2xl:hidden fixed top-3 right-5 lg:left-[21rem] animate-in fade-in">
         <RightSidebarLink jumpTo="about" label="ABOUT" />
