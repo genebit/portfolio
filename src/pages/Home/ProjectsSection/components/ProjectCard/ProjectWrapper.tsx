d4f9ccfbcd7beb2fdacc554da1628dfd8be77ae6
Johcel Gene T. Bitara • BS IT <jbitara@gbox.adnu.edu.ph>
2025-02-12 00:32:48 +0800

    feat: added gallery to cards.

--- src/pages/Home/ProjectsSection/components/ProjectCard/ProjectWrapper.tsx
+++ src/pages/Home/ProjectsSection/components/ProjectCard/ProjectWrapper.tsx
@@ -1,11 +1,12 @@
 import { HTMLAttributes } from "react"
 
-import { MoreHorizontal, ScanSearch } from "lucide-react"
+import { MoreHorizontal } from "lucide-react"
+import ImageGallery from "react-image-gallery"
+import "react-image-gallery/styles/css/image-gallery.css"
 
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { AlertDialog, AlertDialogContent, AlertDialogTrigger } from "@/components/ui/alert-dialog"
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 
 import CardIndicator from "./Card/CardIndicator"
@@ -54,41 +55,9 @@ const ProjectWrapper = ({ thumbnails, children, className, ...props }: ProjectWr
 
   return (
     <div className={compCn} {...props}>
-      <AlertDialog>
-        <AlertDialogTrigger asChild>
-          <Button variant="ghost" className="hover:bg-background" asChild>
-            <div className="flex items-center justify-center h-max">
-              <img
-                src={thumbnails[0]}
-                alt="thumbnail"
-                className="h-max transition-all md:max-w-[20rem] peer hover:cursor-pointer"
-              />
-              <span className="absolute hidden peer-hover:block animate-in fade-in dark:mix-blend-difference dark:grayscale">
-                <ScanSearch />
-              </span>
-            </div>
-          </Button>
-        </AlertDialogTrigger>
-        <AlertDialogContent className="max-w-4xl h-max">
-          <CardCloseButton />
-          <CardIndicator />
-          <Carousel orientation="horizontal" className="max-w-4xl p-0 mx-auto">
-            <CarouselContent>
-              {thumbnails.map((thumbnail, index) => (
-                <CarouselItem key={index}>
-                  <img
-                    src={thumbnail}
-                    alt="thumbnail"
-                    className="w-full transition-all h-max peer hover:cursor-pointer"
-                  />
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <CarouselPrevious variant="secondary" className="dark:mix-blend-difference dark:grayscale" />
-            <CarouselNext variant="secondary" className="dark:mix-blend-difference dark:grayscale" />
-          </Carousel>
-        </AlertDialogContent>
-      </AlertDialog>
+      <div className="w-full md:max-w-72">
+        <ImageGallery slideDuration={1} autoPlay={true} showPlayButton={false} showNav={false} items={thumbnails} />
+      </div>
       <div className="flex flex-col gap-3">{children}</div>
       <ToggleProjectDetails />
     </div>
