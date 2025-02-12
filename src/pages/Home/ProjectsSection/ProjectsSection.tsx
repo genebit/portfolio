552eb1e32aef3383f526688f30ffc2f2bef93c94
Johcel Gene T. Bitara • BS IT <jbitara@gbox.adnu.edu.ph>
2025-02-12 15:46:57 +0800

    feat: added pref. saving.

--- src/pages/Home/ProjectsSection/ProjectsSection.tsx
+++ src/pages/Home/ProjectsSection/ProjectsSection.tsx
@@ -1,4 +1,4 @@
-import { HTMLAttributes } from "react"
+import { HTMLAttributes, useEffect, useState } from "react"
 
 import { Tabs, TabsContent, TabsList } from "@/components/ui/tabs"
 import TabTrigger from "@/pages/Home/ProjectsSection/components/TabTrigger"
@@ -11,6 +11,17 @@ import OtherProjects from "./TabContent/OtherProjects/OtherProjects"
 import ScrollAnimation from "react-animate-on-scroll"
 
 const ProjectsSection = ({ className, ...props }: HTMLAttributes<HTMLElement>) => {
+  const [activeTab, setActiveTab] = useState<string>(() => localStorage.getItem("active-projects-tab") ?? "highlighted")
+
+  const saveActiveTab = (active: string) => {
+    localStorage.setItem("active-projects-tab", active)
+    setActiveTab(active)
+  }
+
+  useEffect(() => {
+    setActiveTab(localStorage.getItem("active-projects-tab") ?? "highlighted")
+  }, [])
+
   return (
     <section className={className} {...props}>
       <ScrollAnimation
@@ -20,9 +31,9 @@ const ProjectsSection = ({ className, ...props }: HTMLAttributes<HTMLElement>) =
         animateOnce={true}
       >
         <header>
-          <h3 className="mb-3 font-bold tracking-widest uppercase font-artegra">PROJECTS</h3>
+          <h3 className="mb-3 font-bold tracking-widest uppercase font-artegra">PROJECTS AND ACHIEVEMENTS</h3>
         </header>
-        <Tabs defaultValue="highlighted" className="w-full md:min-h-[40rem]">
+        <Tabs value={activeTab} onValueChange={saveActiveTab} className="w-full md:min-h-[40rem]">
           <TabsList className="justify-start w-full bg-transparent border-b-2">
             <TabTrigger value="highlighted" label="Highlighted" />
             <TabTrigger value="projects" label="Other Projects" />
